c6c63f66b6c81972203a4f3ccf9fa3a6d5647345
ds6546 <55463801+ds6546@users.noreply.github.com>
2020-03-04 02:00:10 -0600

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,31 @@
+
+import java.net.InetAddress;
+import java.util.Calendar;
+
+public class Client {
+	private InetAddress IPAddress;
+	private Calendar cal;
+	
+    public Client(InetAddress IP, Calendar cal1)
+
+    {
+
+    	this.IPAddress = IP;
+
+        this.cal = cal1;
+
+    }
+    
+    public InetAddress getIP()
+
+    {
+
+    	return IPAddress;
+
+    }
+    
+    public Calendar getCal()
+    {
+    	return cal;
+    }
+}
