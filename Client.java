3ace5c2f48540d6f55c2e2f47fa16fb57a933f4a
Sudip Aryal <38017418+sa8123@users.noreply.github.com>
2020-03-23 12:18:11 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,39 +1,61 @@
-
+package edu.truman.p2p;
 import java.net.InetAddress;
+
 import java.util.Calendar;
 
+
+
 public class Client {
+
 	private InetAddress IPAddress;
+
 	private Calendar cal;
-	private int port;
+
 	
-    public Client(InetAddress IP, Calendar cal1, int port)
+
+    public Client(InetAddress IP, Calendar cal1)
+
+
 
     {
+
+
 
     	this.IPAddress = IP;
 
+
+
         this.cal = cal1;
-        
-        this.port = port;
+
+
 
     }
+
     
+
     public InetAddress getIP()
 
+
+
     {
+
+
 
     	return IPAddress;
 
+
+
     }
+
     
+
     public Calendar getCal()
+
     {
+
     	return cal;
+
     }
-    
-    public int getPort()
-    {
-    	return port;
-    }
+
 }
+
